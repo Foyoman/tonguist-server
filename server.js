b124f2ff000a636576c219b57512d250b9afbf4d
donut <edwardtdo@gmail.com>
2022-09-03 15:17:36 +1000

    user authentication

--- server.js
+++ server.js
@@ -18,6 +18,95 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 routes(app);
+
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('./api/models/userModel');
+
+app.post('/register', async (req, res) => {
+	console.log(req.body)
+	try {
+		const newPassword = await bcrypt.hash(req.body.password, 10)
+		await User.create({
+			name: req.body.name,
+			email: req.body.email,
+			password: newPassword,
+		})
+		res.json({ status: 'ok' })
+	} catch (err) {
+		res.json({ status: 'error', error: 'Duplicate email' })
+	}
+})
+
+app.post('/login', async (req, res) => {
+	console.log(req.body)
+	const user = await User.findOne({ 
+		email: req.body.email, 
+	})
+
+	if (!user) { 
+		return {
+			status: 'error',
+			error: 'Invalid login'
+		}
+	}	
+
+	const isPasswordValid = await bcrypt.compare(req.body.password, user.password)
+
+	if (isPasswordValid) {
+
+		const token = jwt.sign(
+			{
+				name: user.name,
+				email: user.email,
+			}, 
+			'secret123'
+		)
+
+		return res.json({ status: 'ok', user: token })
+	} else {
+		return res.json({ status: 'error', user: false })
+	}
+})
+
+app.get('/dashboard', async (req, res) => {
+
+	const token = req.headers['x-access-token']
+
+	try {
+		const decoded = jwt.verify(token, 'secret123')
+		const email = decoded.email
+		await User.updateOne(
+			{ email: email }
+		)
+
+		return res.json({ status: 'ok' })
+	} catch (error) {
+		console.log(error)
+		res.json({ status: 'error', error: 'invalid token' })
+	}
+
+	const user = await User.findOne({ 
+		email: req.body.email, 
+		password: req.body.password,
+	})
+
+	if (user) {
+
+		const token = jwt.sign(
+			{
+				name: user.name,
+				email: user.email,
+			}, 
+			'secret123'
+		)
+
+		return res.json({ status: 'ok', user: token })
+	} else {
+		return res.json({ status: 'error', user: false })
+	}
+})
+
 app.listen(port);
 
 app.use((req, res) => {
